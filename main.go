f02d844323b54dbafa63f653425fb7298d35dd57
Myroslava <tarcham1993@gmail.com>
2022-06-20 19:46:19 +0200

    types and pointers

--- main.go
+++ main.go
@@ -4,9 +4,26 @@ import "fmt"
 
 func main() {
 	var conferenceName = "Go Conference"
-	const conferenceTicketsCount = 50
-	var remainingTickets = 50
+	const conferenceTicketsCount uint = 50
+	var remainingTickets int64 = 50
 	fmt.Println("Welcome to", conferenceName, "booking application")
 	fmt.Println("We have total of", conferenceTicketsCount, "tickets and", remainingTickets, "are still available")
 	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	userName = "anonimus"
+
+	fmt.Println("Please enter your name:")
+	fmt.Scan(&userName)
+	var userTickets int = 2
+
+	pointerToUserName := &userName
+	pointerToUserTickets := &userTickets
+
+	fmt.Printf(
+		"User %v, type of string pointer %T, type of int pointer %T, pointer value %v",
+		userName,
+		pointerToUserName,
+		pointerToUserTickets,
+		pointerToUserName)
 }
